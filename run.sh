0460e55d89b19ffe4c0feba03998b165aaa6f448
Lukas Poustka <lupo01@lns-87004.sb.dfki.de>
2013-02-15 21:37:21 +0100

    Changed variables in config/properties, features using tree-tagger were commented out in feature_blackbox2.xml, deleted libraries that can be downloaded from the Internet, deleted some lang_resources files that can be downloaded from FTP, modified run.sh - FeatureExtractor.java is launched.

--- run.sh
+++ run.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 # Remove any intruding character (still necessary for native UTF-8 files)
-iconv --from-code=UTF-8 --to-code=ISO-8859-1//TRANSLIT input/source.en | iconv --from-code=ISO-8859-1 --to-code=UTF-8//TRANSLIT --output input/source.en
-iconv --from-code=UTF-8 --to-code=ISO-8859-1//TRANSLIT input/target.es | iconv --from-code=ISO-8859-1 --to-code=UTF-8//TRANSLIT --output input/target.es
-java -Xmx1g -XX:+UseConcMarkSweepGC -classpath build/classes:lib/commons-cli-1.2.jar:lib/stanford-postagger.jar:lib/BerkeleyParser-1.7.jar FeatureExtractorSimple -lang english spanish -input input/source.en input/target.es -mode bb -config config/config_en-es.properties
+#iconv --from-code=UTF-8 --to-code=ISO-8859-1//TRANSLIT input/source.en | iconv --from-code=ISO-8859-1 --to-code=UTF-8//TRANSLIT --output input/source.en
+#iconv --from-code=UTF-8 --to-code=ISO-8859-1//TRANSLIT input/target.es | iconv --from-code=ISO-8859-1 --to-code=UTF-8//TRANSLIT --output input/target.es
+#java -Xmx1g -XX:+UseConcMarkSweepGC -classpath build/classes:lib/commons-cli-1.2.jar:lib/stanford-postagger.jar:lib/BerkeleyParser-1.7.jar FeatureExtractorSimple -lang english spanish -input input/source.en input/target.es -mode bb -config config/config_en-es.properties
+java -Xmx1g -XX:+UseConcMarkSweepGC -classpath build/classes:lib/commons-cli-1.2.jar:lib/stanford-postagger.jar:lib/BerkeleyParser-1.7.jar wlv.mt.enes.FeatureExtractor -lang english spanish -input input/source.en input/target.es -mode bb -config config/config_en-es.properties
