49c8503fcdbea14298c073e96c1296965a8845c2
lupo01 <lukas.poustka@dfki.de>
2013-08-01 17:11:07 +0200

    Giza debugging.

--- src/shef/mt/tools/FileModel.java
+++ src/shef/mt/tools/FileModel.java
@@ -84,7 +84,6 @@ public class FileModel {
     }
 
     public static boolean containsWord(String word) {
-    	System.out.println(words.isEmpty());
         return words.containsKey(word);
     }
 
