9e714acc45f98eba4a1d121e2987a9537e0e8825
carolscarton <carol.scarton@gmail.com>
2013-09-10 16:05:09 +0100

    v1

--- src/shef/mt/features/impl/bb/Feature9987.java
+++ src/shef/mt/features/impl/bb/Feature9987.java
@@ -13,7 +13,7 @@ public class Feature9987 extends Feature {
 
 	@Override
 	public void run(Sentence source, Sentence target) {
-                Integer qualityValue = (Integer) source.getValue("terminology");
+                Integer qualityValue = (Integer) target.getValue("terminology");
 		setValue(new Float(qualityValue));
 
 	}
